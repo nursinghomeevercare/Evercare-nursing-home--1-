5bdcadd38f283f1909721b301d6512941889e959
Siroros Roongdonsai <siroros_okdaika@hotmail.com>
2026-03-03 15:02:33 +0700

    Add fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,8 +8,9 @@ export default defineConfig({
     // The React and Tailwind plugins are both required for Make, even if
     // Tailwind is not being actively used – do not remove them
     react(),
-    tailwindcss(),
+    tailwindcss(),    
   ],
+  base: 'Evercare-nursing-home--1-/',
   resolve: {
     alias: {
       // Alias @ to the src directory
