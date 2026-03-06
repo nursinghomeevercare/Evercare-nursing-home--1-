5fe0ae2f5eb2992939bf9e4e5ca79d28746b90f3
Siroros Roongdonsai <siroros_okdaika@hotmail.com>
2026-03-06 11:49:46 +0700

    fixed path in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     react(),
     tailwindcss(),    
   ],
-  base: '/Evercare-nursing-home--1-/',
+  base: '/',
   resolve: {
     alias: {
       // Alias @ to the src directory
