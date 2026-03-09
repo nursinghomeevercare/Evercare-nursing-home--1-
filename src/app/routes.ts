f7b720f90fca350842523d40cfe2d19fa99f0f91
Siroros Roongdonsai <siroros_okdaika@hotmail.com>
2026-03-09 16:11:41 +0700

    routing fix

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -13,26 +13,26 @@ import BlogPage from "./pages/BlogPage";
 import ContactPage from "./pages/ContactPage";
 import NotFound from "./pages/NotFound";
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    Component: Layout,
-    children: [
-      { index: true, Component: HomePage },
-      { path: "nursing-home-bangkok", Component: NursingHomePage },
-      { path: "dementia-care", Component: DementiaCare },
-      { path: "bedbound-care", Component: BedboundCare },
-      { path: "rehabilitation", Component: RehabilitationCare },
-      { path: "pricing", Component: PricingPage },
-      { path: "facilities", Component: FacilitiesPage },
-      { path: "international", Component: InternationalPage },
-      { path: "about", Component: AboutPage },
-      { path: "blog", Component: BlogPage },
-      { path: "contact", Component: ContactPage },
-      { path: "*", Component: NotFound },
-      ],
-    },
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      Component: Layout,
+      children: [
+        { index: true, Component: HomePage },
+        { path: "nursing-home-bangkok", Component: NursingHomePage },
+        { path: "dementia-care", Component: DementiaCare },
+        { path: "bedbound-care", Component: BedboundCare },
+        { path: "rehabilitation", Component: RehabilitationCare },
+        { path: "pricing", Component: PricingPage },
+        { path: "facilities", Component: FacilitiesPage },
+        { path: "international", Component: InternationalPage },
+        { path: "about", Component: AboutPage },
+        { path: "blog", Component: BlogPage },
+        { path: "contact", Component: ContactPage },
+        { path: "*", Component: NotFound }
+      ]
+    }
   ],
-  {
-    basename: "/"  }
+  { basename: "/" }
 );
