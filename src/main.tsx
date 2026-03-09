e768fd95a6659f90ba7f72b38546d7aa1e95581b
Siroros Roongdonsai <siroros_okdaika@hotmail.com>
2026-03-09 16:00:22 +0700

    fix routing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,12 @@
   import { createRoot } from "react-dom/client";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+const redirect = sessionStorage.redirect;
+
+if (redirect) {
+  delete sessionStorage.redirect;
+  window.history.replaceState(null, null, redirect);
+}
 
   createRoot(document.getElementById("root")!).render(<App />);
   
