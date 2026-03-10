300a00e996ad75828ea65d06dc494fedda33b4ba
Siroros Roongdonsai <siroros_okdaika@hotmail.com>
2026-03-10 10:33:54 +0700

    Fix: Routing and redirect structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,14 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+// Comment this out for a moment
+/*
 const redirect = sessionStorage.redirect;
-
 if (redirect) {
   delete sessionStorage.redirect;
   window.history.replaceState(null, null, redirect);
 }
+*/
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(<App />);
